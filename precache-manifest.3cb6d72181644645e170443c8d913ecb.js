f44fe8469a3e75daf9ee762b1f310613a686c9b5
Rushali Asthana <rushaliasthana@gmail.com>
2020-07-31 18:34:58 +0530

    Updates

---
+++ precache-manifest.3cb6d72181644645e170443c8d913ecb.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19270fa295f4667fed053ef23962443",
+    "url": "/MusicTherapyMobile/index.html"
+  },
+  {
+    "revision": "3942e2e6ba0ea5610059",
+    "url": "/MusicTherapyMobile/static/css/main.cec260d6.chunk.css"
+  },
+  {
+    "revision": "b9713881cfb392f6c3ae",
+    "url": "/MusicTherapyMobile/static/js/2.6da079f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MusicTherapyMobile/static/js/2.6da079f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3942e2e6ba0ea5610059",
+    "url": "/MusicTherapyMobile/static/js/main.66fccb56.chunk.js"
+  },
+  {
+    "revision": "6453c8a996b9b99d4b3e",
+    "url": "/MusicTherapyMobile/static/js/runtime-main.fa698358.js"
+  },
+  {
+    "revision": "0467922e0817d13a434e20c3be5383b1",
+    "url": "/MusicTherapyMobile/static/media/DilIbaadat.0467922e.mp3"
+  },
+  {
+    "revision": "f5232b01c56264a55cd3595c45196005",
+    "url": "/MusicTherapyMobile/static/media/DilIbadat.f5232b01.jpg"
+  },
+  {
+    "revision": "23bdc8fde7beec3c3c12a072614cab11",
+    "url": "/MusicTherapyMobile/static/media/KMPH.23bdc8fd.mp3"
+  },
+  {
+    "revision": "f9304908fce967fde9464246735f82bf",
+    "url": "/MusicTherapyMobile/static/media/KMPH.f9304908.jpg"
+  },
+  {
+    "revision": "881946ba5fe19310b43c8e2fdb147222",
+    "url": "/MusicTherapyMobile/static/media/Kamli.881946ba.mp3"
+  },
+  {
+    "revision": "e25d677de0713533e6b5e52e87923c8b",
+    "url": "/MusicTherapyMobile/static/media/LKLP.e25d677d.jpg"
+  },
+  {
+    "revision": "19891678b7facf5acc6ffe68c326f180",
+    "url": "/MusicTherapyMobile/static/media/LabonKo.19891678.mp3"
+  },
+  {
+    "revision": "2a4672a16ff95b7d39a8642a42a08d0b",
+    "url": "/MusicTherapyMobile/static/media/Muqabla.2a4672a1.mp3"
+  },
+  {
+    "revision": "a0582c5a697ed3126ab79af1071ebeaa",
+    "url": "/MusicTherapyMobile/static/media/StreetDancer.a0582c5a.jpg"
+  },
+  {
+    "revision": "87423c72df60a3b74585dd26daa75d99",
+    "url": "/MusicTherapyMobile/static/media/Tum Se Hi.87423c72.mp3"
+  },
+  {
+    "revision": "26dda61f3f5aa0521ecdf1b4639cf346",
+    "url": "/MusicTherapyMobile/static/media/kamli.26dda61f.jpg"
+  },
+  {
+    "revision": "82d43ca627072cef49ba33bce7e235c8",
+    "url": "/MusicTherapyMobile/static/media/landingbg.82d43ca6.png"
+  },
+  {
+    "revision": "c68547da19a56eda6a458c9effe26508",
+    "url": "/MusicTherapyMobile/static/media/logo.c68547da.png"
+  },
+  {
+    "revision": "434e8635590ba88aa47ca1fbdd26948e",
+    "url": "/MusicTherapyMobile/static/media/logo1.434e8635.png"
+  },
+  {
+    "revision": "0844543ca722a02820ea4e35d7d5b73b",
+    "url": "/MusicTherapyMobile/static/media/sound.0844543c.gif"
+  },
+  {
+    "revision": "398647358e7efa8375d7767948e9aded",
+    "url": "/MusicTherapyMobile/static/media/tumsehi.39864735.jpg"
+  }
+]);
